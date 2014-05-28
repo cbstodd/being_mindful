814ffe5166c7c034503c3769220ca51effac7bdf
Colin Stodd <colin@Colins-MacBook-Pro.local>
2014-05-28 09:46:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeingMindful::Application.config.secret_key_base = '55cdc0ef0368efd098502a28cc6a13b38fc5dc35f3f232ab43959c4d8ecc1f15e77190503989bea73eaf2279a24be2bba9ae17c8e64e46b579cf685dafd5e96e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeingMindful::Application.config.session_store :cookie_store, key: '_being_mindful_session'
