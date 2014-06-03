91bc4646fbb382cdd274e21a735a262692362ae6
Colin Stodd <cbstodd@gmail.com>
2014-06-03 10:52:04 -0400

    Finished sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Being Mindful #{@user.name}"
+      flash[:success] = "Welcome to Being Mindful #{@user.name}!"
       redirect_to @user
     else
       render 'new'
