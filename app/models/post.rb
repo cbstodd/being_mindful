8cf10abb6bdb00a9d65f6469b4a50fc279f9b43e
Colin Stodd <cbstodd@gmail.com>
2014-08-19 13:39:10 -0400

    Finished following users

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 3000, minimum: 5 }
   
 end
