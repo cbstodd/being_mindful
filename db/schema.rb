bfcf8d4bf27f2d26a3ba7882cce1197d7543475f
Colin Stodd <cbstodd@gmail.com>
2014-08-26 16:33:39 -0400

    Deleted Carrierwave and supporting files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826024954) do
+ActiveRecord::Schema.define(version: 20140819152551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20140826024954) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
   end
 
   create_table "relationships", force: true do |t|
