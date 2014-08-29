b22e9fe5bd58080d089b63a69aab8267a6c7085e
Colin Stodd <cbstodd@gmail.com>
2014-08-29 13:19:45 -0400

    Added Background CSS and fixed ActionMailer on signup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,4 @@
-class UserMailer < ActionMailer::Base
+ class UserMailer < ActionMailer::Base
   default from: "support@pacific-journey-3467.herokuapp.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -8,10 +8,8 @@ class UserMailer < ActionMailer::Base
   #
   def welcome_email(user)
     @user = user
-    
-    @greeting = "Hey"
-    @url = "pacific-journey-3467.herokuapp.com"
 
+    @url = "pacific-journey-3467.herokuapp.com"
     mail to: "user.email", subject: "Being Mindful [welcome]"
   end
 end
