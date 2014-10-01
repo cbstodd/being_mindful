b71137666925c9ddaa524ac73b716574a4bf9730
Colin Stodd <cbstodd@gmail.com>
2014-10-01 13:34:48 -0400

    update cache location

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
-    
+
   }
+  config.cache_dir = "#{Rails.root}/tmp/"
   config.fog_directory  = 'beingmindful-posts'                     # required
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
