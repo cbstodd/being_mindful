e8e46ee83814beeacfcac55ed7cb8be32a1ce90a
Colin Stodd <cbstodd@gmail.com>
2014-10-02 14:41:28 -0400

    Fog credentials

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,8 +9,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   process :set_content_type
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [350, 350]
+  process :resize_to_fit => [350, 350]
   #
   # def scale(width, height)
   #   # do something
