d048ea072b22094e38eb49bc9d958a93102e418e
Colin Stodd <cbstodd@gmail.com>
2014-10-02 15:08:11 -0400

    turned off bootsy fog

--- config/initializers/bootsy.rb
+++ config/initializers/bootsy.rb
@@ -55,7 +55,7 @@ Bootsy.setup do |config|
   # initializer file, as described in Carrierwave's docs:
   # https://github.com/carrierwaveuploader/carrierwave/blob/master/README.md#using-amazon-s3
   #
-  config.storage = :fog
+  # config.storage = :fog
 
 
   # Store directory (inside 'public') for storage = :file
