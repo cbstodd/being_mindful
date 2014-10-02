e391377b3d18511965f0d88c6cd350968f80d987
Colin Stodd <cbstodd@gmail.com>
2014-10-02 15:03:47 -0400

    missing ',' after last paramater

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AWS_ACCESS_KEY_ID',                        # required
-    :aws_secret_access_key  => 'AWS_SECRET_ACCESS_KEY',
-    :region                 => 'us-east-1'                        # required
+    :aws_secret_access_key  => 'AWS_SECRET_ACCESS_KEY'
 
   }
   config.cache_dir = "#{Rails.root}/tmp/"
   config.fog_directory  = 'beingmindful-posts'                     # required
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.storage = :fog
 end
