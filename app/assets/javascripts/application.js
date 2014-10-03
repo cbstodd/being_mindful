3cfa6cccc26e48d614d86f72b942d346da63464d
Colin Stodd <cbstodd@gmail.com>
2014-10-02 22:21:05 -0400

    fix s3 bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,41 +17,4 @@
 //= require_tree .
 
 
-$(function() {
-  $('.directUpload').find("input:file").each(function(i, elem) {
-    var fileInput    = $(elem);
-    console.log(fileInput);
-  });
-});
 
-$(function() {
-  $('.directUpload').find("input:file").each(function(i, elem) {
-    var fileInput    = $(elem);
-    var form         = $(fileInput.parents('form:first'));
-    var submitButton = form.find('input[type="submit"]');
-    var progressBar  = $("<div class='bar'></div>");
-    var barContainer = $("<div class='progress'></div>").append(progressBar);
-    fileInput.after(barContainer);
-  });
-});
-
-$(function() {
-  $('.directUpload').find("input:file").each(function(i, elem) {
-    var fileInput    = $(elem);
-    var form         = $(fileInput.parents('form:first'));
-    var submitButton = form.find('input[type="submit"]');
-    var progressBar  = $("<div class='bar'></div>");
-    var barContainer = $("<div class='progress'></div>").append(progressBar);
-    fileInput.after(barContainer);
-    fileInput.fileupload({
-      fileInput:       fileInput,
-      url:             '<%= @s3_direct_post.url %>',
-      type:            'POST',
-      autoUpload:       true,
-      formData:         <%= @s3_direct_post.fields.to_json.html_safe %>,
-      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
-      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
-      replaceFileInput: false
-    });
-  });
-});
